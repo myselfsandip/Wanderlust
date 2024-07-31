659f7c9b7a72f30596146f7c82f7cf144a4a37e8
myselfsandip <sandipsingha714@gmail.com>
2024-07-31 21:09:07 +0530

    Added Pre-loader

--- init/index.js
+++ init/index.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://sandip:Sandip%401206++@cluster0.2qgyl30.mongodb.net/";
+
 main().then(() => {
     console.log("DB Connection Successfull");
 }).catch(err => {
